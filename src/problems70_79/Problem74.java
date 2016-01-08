1a7b2cad42b178812212774a18b6203d43b92269
chris <chriskjellqvis@gmail.com>
2016-01-07 20:40:06 -0600

    Problem 74: This problem was actually pretty easy. It's a straight forward approach with no shortcuts. There's really not much to it. I tried writing it in Scala too but it never compiled for some reason. I think it's because I attempted using recursion for finding the chain lengths and that resulted in a REALLY big stack, which made it compile slowly? It's kind of disappointing but I'm still learning. I might post the code in a comment to this code later.

---
+++ src/problems70_79/Problem74.java
@@ -0,0 +1,47 @@
+package problems70_79;
+
+import java.util.ArrayList;
+
+/**
+ * Created by chris on 12/26/15.
+ */
+public class Problem74 {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i < 1000000; i++) {
+            if (findChainLength(i) == 60) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static int factorial(int n) {
+        int acc = 1;
+        for (int i = 2; i <= n; i++) {
+            acc *= i;
+        }
+        return acc;
+    }
+
+    public static int digitFactorialSum(String str) {
+        int sum = 0;
+        char[] ar = str.toCharArray();
+        for (char a : ar) {
+            sum += factorial(Character.getNumericValue(a));
+        }
+        return sum;
+    }
+
+    public static int findChainLength(int n) {
+        ArrayList<Integer> previousOccurences = new ArrayList<>();
+        int len = 0;
+        while (!previousOccurences.contains(n)) {
+            previousOccurences.add(n);
+            n = digitFactorialSum(Integer.toString(n));
+            len++;
+        }
+        return len;
+    }
+}
+
